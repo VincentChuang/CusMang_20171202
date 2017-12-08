abd69e75d7239dfda732594311396c50d68694f9
vincentchuang <laba.chuang@gmail.com>
2017-12-08 11:11:01 +0800

    自訂輸入驗證 會 錯誤，需詢問老師

--- CusMang/Models/客戶資料.Partial.cs
+++ CusMang/Models/客戶資料.Partial.cs
@@ -4,6 +4,7 @@ namespace CusMang.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
+    //using DataType;
 
     [MetadataType(typeof(客戶資料MetaData))]
     public partial class 客戶資料
@@ -25,6 +26,7 @@ namespace CusMang.Models
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
         [Required]
+        //[自訂行動電話驗證]
         public string 電話 { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
